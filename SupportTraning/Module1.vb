3d6be436fb7491b72225a8dcc1f632f3c53748ce
Udayakantha <igudaya@yahoo.com>
2014-04-12 14:28:59 +0530

    vb.net console

---
+++ SupportTraning/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        For index = 1 To 100
+            If index Mod 3 = 0 And index Mod 5 = 0 Then
+                Console.WriteLine("SupportTraining")
+            ElseIf index Mod 3 = 0 Then
+                Console.WriteLine("Support")
+            ElseIf index Mod 5 = 0 Then
+                Console.WriteLine("Training")
+            Else
+                Console.WriteLine(index)
+            End If
+
+        Next
+
+    End Sub
+
+End Module
